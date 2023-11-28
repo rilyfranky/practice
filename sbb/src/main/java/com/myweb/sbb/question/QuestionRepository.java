9746257906dd885c54bcd1b85f1ed34c48554231
yihyunsuk <nicecat0991@gmail.com>
2023-11-28 14:24:40 +0900

    Specification 대신 Query 어노테이션 사용

--- sbb/src/main/java/com/myweb/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/myweb/sbb/question/QuestionRepository.java
@@ -17,4 +17,18 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Page<Question> findAll(Pageable pageable);
     Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 
+    //Specification 대신 어노테이션을 이용해 직접 쿼리를 작성하는 방법
+    @Query("select " +
+        "distinct q " +
+        "from Question q " +
+        "left outer join SiteUser u1 on q.author=u1 " +
+        "left outer join Answer a on a.question=q " +
+        "left outer join SiteUser u2 on a.author=u2 " +
+        "where " +
+        "   q.subject like %:kw% " +
+        "   or q.content like %:kw% " +
+        "   or u1.username like %:kw% " +
+        "   or a.content like %:kw% " +
+        "   or u2.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
